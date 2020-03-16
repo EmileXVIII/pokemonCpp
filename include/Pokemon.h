e3ca5ef369da8af05a8987d8f3d55ea0813b3d9d
Emile <emile.roche@ynov.com>
2020-03-16 10:42:41 +0100

    initial commit

---
+++ include/Pokemon.h
@@ -0,0 +1,21 @@
+
+#include <iostream>
+class Pokemon
+{
+private:
+    std::string name;
+    int hp;
+    std::string type;
+    uint xp;
+public:
+    Pokemon(/* args */);
+    ~Pokemon();
+};
+
+Pokemon::Pokemon(/* args */)
+{
+}
+
+Pokemon::~Pokemon()
+{
+}
